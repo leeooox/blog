47302780159c5a50f0479b2bd4646eb7898802e8
liniu <leeooox@gmail.com>
2021-07-29 00:14:02 +0800

    first gh-pages for test

---
+++ tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages":[{"title":"第一篇gitpages blog","text":"开篇第一个文章 采用pelican写作，发布在gitpage上。","tags":"misc","url":"https://blog.sipi.tech/my-fist-post.html","loc":"https://blog.sipi.tech/my-fist-post.html"}]};
